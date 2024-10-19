473fb97bf5f076eee368cb65e28d0b8f76e46de0
ira <mail@ira.pink>
2024-10-19 15:55:42 +0300

    added error handling and logging

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const config = require('./utils/config')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const { errorHandler, unknownEndpoint } = require('./utils/middlewares')
 const mongoose = require('mongoose')
+
+morgan.token('body', (req) => req.method === 'POST'
+  ? JSON.stringify(req.body)
+  : ' '
+)
 
 mongoose.set('strictQuery', false)
 
@@ -17,7 +24,14 @@ mongoose
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(morgan(
+`:method :url 
+:status :res[content-length] - :response-time ms
+:body`))
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 module.exports = app
---
+++ utils/middlewares.js
@@ -0,0 +1,22 @@
+const { info, error } = require('./logger')
+
+const unknownEndpoint = (req, res) =>
+  res.status(404).send({ error: 'unknown endpoint' })
+
+
+const errorHandler = (e, req, res, n) => {
+  error(e.message)
+  if (e.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  if (e.name === 'MissingContent') {
+    return res.status(400).send({ error: 'missing content' })
+  }
+  if (e.name === 'ValidationError') {
+    return res.status(400).send({ error: e.message })
+  }
+  n(e)
+}
+
+module.exports = { unknownEndpoint, errorHandler }
+
